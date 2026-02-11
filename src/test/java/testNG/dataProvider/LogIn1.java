b65fb3352b468ebb3d543d98a8e16dabcbc7284e
Mahmoud Abdallah Hassan <Mahmoud-Abdallah@shakeregypt.com>
2026-02-11 11:53:24 +0200

    Last update

--- src/test/java/testNG/dataProvider/LogIn1.java
+++ src/test/java/testNG/dataProvider/LogIn1.java
@@ -3,10 +3,16 @@ package testNG.dataProvider;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LogIn1
 {
 
-    @Test (dataProvider = "logInData")
+    @Test (dataProvider = "logInDataCSV")
     public void logIn (String email, String password, boolean success)
     {
         System.out.println("Log In Credentials: " + "\n" +
@@ -25,5 +31,24 @@ public class LogIn1
         data [2] [0] = "Test@Test.com";		        data [2] [1] = "Test1234";			data [2] [2] = false;
 
         return data;
+    }
+    @DataProvider
+    public Object[][] logInDataCSV(){
+        String filePath = "resources/DP.csv";
+        List<Object[]> dataList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            bufferedReader.readLine();
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] values = line.split(",");
+                String email = values[0];
+                String password = values[1];
+                boolean expectedResult = Boolean.parseBoolean(values[2]);
+                dataList.add(new Object[]{email, password, expectedResult});
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dataList.toArray(new Object[0][0]);
     }
 }
